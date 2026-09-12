a3a933fe8f2373ed162717945e203e3b87c09e75
Emanuel <e.dziedzic.kontakt@gmail.com>
2026-09-12 21:37:31 +0200

    feat: add news formatting function and orchestrator

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if not api_key:
     print("Error: FINNHUB_API_KEY is not found in file .env")
     exit(1)
 
-def get_company_news(ticker,date_from,date_to):
+def get_company_news(ticker: str,date_from: str,date_to: str):
     params = {"symbol": ticker,
               "from": date_from,
               "to": date_to,
@@ -21,9 +21,28 @@ def get_company_news(ticker,date_from,date_to):
     except requests.exceptions.Timeout:
         print(f"Error: Server has not responded within 5 seconds for {ticker}.")
     except requests.exceptions.HTTPError as e:
-        print(f"Error HTTP {data.status_code}: {e}")
+        print(f"Error HTTP {e.response.status_code}: {e}")
     except requests.exceptions.RequestException as e:
         print(f"Destination Error: {e}")
     except ValueError:
         print("Error: Destination server response is not correct JSON format")
     return []
+
+def format_news(companies: list,ticker: str):
+    result = []
+    for headline in companies:
+        result.append({'id': headline.get('id', ''),
+                    'ticker':ticker,
+                    'summary':headline.get('summary','')})
+    return result
+
+def clean_news_orchestrator(ticker: str,date_from: str,date_to: str):
+    raw_data = get_company_news(ticker,date_from,date_to)
+    clean_data = format_news(raw_data,ticker)
+    return clean_data
+
+if __name__ == "__main__":
+    test_news = clean_news_orchestrator("UBER", "2026-09-01", "2026-09-12")
+    print(f"Results: {len(test_news)}")
+    if test_news:
+        print("First result:", test_news[0])
