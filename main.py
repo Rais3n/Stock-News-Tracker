3300f04687f0c8521d68f28bb5fe9d6692ce4fab
Emanuel <e.dziedzic.kontakt@gmail.com>
2026-09-12 20:30:12 +0200

    feat: add api key reader from local env

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import os 
+import requests
+from dotenv import load_dotenv
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+load_dotenv()
+api_key = os.getenv("FINNHUB_API_KEY")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if not api_key:
+    print("Error: FINNHUB_API_KEY is not found in file .env")
+    exit(1)
