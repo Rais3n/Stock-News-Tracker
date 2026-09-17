1e7af8f6d65b0d95461ea16329925c27046a3633
Emanuel <e.dziedzic.kontakt@gmail.com>
2026-09-17 14:45:21 +0200

    pref: normalize vectors in create_embedding function

--- rag/embeddings.py
+++ rag/embeddings.py
@@ -3,4 +3,4 @@ from sentence_transformers import SentenceTransformer
 model = SentenceTransformer("BAAI/bge-m3")
 
 def create_embedding(chunk):
-    return model.encode(chunk)
+    return model.encode(chunk,normalize_embeddings=True)
